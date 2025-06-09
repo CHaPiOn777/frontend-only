7c92878038955c13fbbfeb4758f464f506f5ee83
VladislavSh <vladislavshishkin123@gmail.com>
2025-06-09 20:11:42 +0300

    vercel

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,4 +1,3 @@
-// src/styles/GlobalStyles.ts
 import { createGlobalStyle } from "styled-components";
 import "@fontsource/bebas-neue/400.css";
 import "@fontsource/pt-sans";
@@ -9,15 +8,11 @@ const GlobalStyles = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-
   
   html, body {
     font-family: 'PT Sans', sans-serif;
     background-color: #f4f5f9;
-    
   }
-
-
 `;
 
 export default GlobalStyles;
