b28c5b1b5cfaae16fbc9ec29e49cb94e20741078
VladislavSh <vladislavshishkin123@gmail.com>
2025-06-09 20:30:36 +0300

    build1

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,19 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     clean: true,
-    publicPath: "/",
+    publicPath: "/", // важно: все ассеты от корня
   },
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
+    alias: { "@": path.resolve(__dirname, "src") },
     extensions: [".tsx", ".ts", ".js"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-      filename: "index.html",
-    }),
-  ],
-  devServer: {
-    static: { directory: path.resolve(__dirname, "public") },
-    port: 3000,
-    hot: true,
-    open: true,
-    historyApiFallback: true,
   },
   module: {
     rules: [
@@ -40,10 +27,44 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif|ico|webp)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
     ],
   },
+  plugins: [
+    // Генерим index.html + инжектим <script src="bundle.js">
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      inject: "body",
+    }),
+    // Копируем из public всё, кроме index.html
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public"),
+          filter: async (resourcePath) => !resourcePath.endsWith("index.html"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"), // теперь сервим свою сборку
+    },
+    port: 3000,
+    hot: true,
+    open: true,
+    historyApiFallback: true, // SPA: все маршруты на index.html
+  },
   stats: {
-    children: true, // показывать логи дочерних сборок
-    errorDetails: true, // подробности ошибок
+    children: true,
+    errorDetails: true,
   },
 };
